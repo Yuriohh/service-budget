813994abb4d2912f953fc9c8e020f02e60ada975
Yuri Santos de Lima <yuri@redeparcerias.com>
2026-03-17 22:53:10 -0300

    feat: Implement design system with custom fonts, colors, and icon library, and add related dependencies.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
+import {
+  Lato_400Regular,
+  Lato_700Bold,
+  useFonts,
+} from "@expo-google-fonts/lato";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
 import "./global.css";
 import { HomeScreen } from "./src/screens/HomeScreen";
-
 const Stack = createNativeStackNavigator();
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,45 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      // Aqui você inserirá as cores do seu Figma posteriormente
+      colors: {
+        main: {
+          purpleLight: "#DFDAF2",
+          purpleBase: "#6A46EB",
+        },
+        base: {
+          gray100: "#FAFAFA",
+          gray200: "#F0F0F0",
+          gray300: "#E6E5E5",
+          gray400: "#A1A2A1",
+          gray500: "#676767",
+          gray600: "#4A4A4A",
+          gray700: "#0F0F0F",
+        },
+        feedback: {
+          dangerLight: "#FFD6D6",
+          dangerBase: "#DB4D4D",
+          dangerDark: "#9E4949",
+          successLight: "#BFF7BE",
+          successBase: "#4BB84A",
+          successDark: "#30752F",
+          infoLight: "#CEEFFF",
+          infoBase: "#2AA1D9",
+          infoDark: "#1D7096",
+        },
+      },
+      fontFamily: {
+        regular: ["Lato_400Regular"],
+        bold: ["Lato_700Bold"],
+      },
+      fontSize: {
+        "title-lg": ["18px", "140%"],
+        "title-md": ["16px", "140%"],
+        "title-sm": ["14px", "140%"],
+        "title-xs": ["12px", "140%"],
+        "text-md": ["16px", "140%"],
+        "text-sm": ["14px", "140%"],
+        "text-xs": ["12px", "140%"],
+      },
     },
   },
   plugins: [],
