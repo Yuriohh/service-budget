d3bc2f97020f95e10c7045219232c91c0f46a058
Yuri Santos de Lima <yuriohh2@gmail.com>
2026-03-23 10:00:39 -0300

    feat: create theme colors

---
+++ src/themes/colors.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  main: {
+    purpleLight: "#DFDAF2",
+    purpleBase: "#6A46EB",
+  },
+  base: {
+    gray100: "#FAFAFA",
+    gray200: "#F0F0F0",
+    gray300: "#E6E5E5",
+    gray400: "#A1A2A1",
+    gray500: "#676767",
+    gray600: "#4A4A4A",
+    gray700: "#0F0F0F",
+  },
+  feedback: {
+    dangerLight: "#FFD6D6",
+    dangerBase: "#DB4D4D",
+    dangerDark: "#9E4949",
+    successLight: "#BFF7BE",
+    successBase: "#4BB84A",
+    successDark: "#30752F",
+    infoLight: "#CEEFFF",
+    infoBase: "#2AA1D9",
+    infoDark: "#1D7096",
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { colors } from "./src/themes/colors";
+
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
@@ -7,32 +9,7 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      colors: {
-        main: {
-          purpleLight: "#DFDAF2",
-          purpleBase: "#6A46EB",
-        },
-        base: {
-          gray100: "#FAFAFA",
-          gray200: "#F0F0F0",
-          gray300: "#E6E5E5",
-          gray400: "#A1A2A1",
-          gray500: "#676767",
-          gray600: "#4A4A4A",
-          gray700: "#0F0F0F",
-        },
-        feedback: {
-          dangerLight: "#FFD6D6",
-          dangerBase: "#DB4D4D",
-          dangerDark: "#9E4949",
-          successLight: "#BFF7BE",
-          successBase: "#4BB84A",
-          successDark: "#30752F",
-          infoLight: "#CEEFFF",
-          infoBase: "#2AA1D9",
-          infoDark: "#1D7096",
-        },
-      },
+      colors: colors,
       fontFamily: {
         regular: ["Lato_400Regular"],
         bold: ["Lato_700Bold"],
