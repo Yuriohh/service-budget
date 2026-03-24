e2ae7e6aa68296da5da3b97527bd6a2daa081d7d
Yuri Santos de Lima <yuriohh2@gmail.com>
2026-03-24 10:48:30 -0300

    refac: status badge in budget card

--- src/components/BudgetCard/index.tsx
+++ src/components/BudgetCard/index.tsx
@@ -1,40 +1,12 @@
 import { Budget } from "@/src/types/budget";
-import { cn } from "@/src/utils/cn";
 import { Text, View } from "react-native";
+import { StatusBadge } from "../BudgetStatus";
 
 type BudgetCardProps = {
   data: Budget;
 };
 
-const STATUS_STYLE = {
-  draft: {
-    bg: "bg-base-gray300",
-    dot: "bg-base-gray500",
-    text: "text-base-gray600",
-    label: "Rascunho",
-  },
-  approved: {
-    bg: "bg-feedback-successLight",
-    dot: "bg-feedback-successBase",
-    text: "text-feedback-successDark",
-    label: "Aprovado",
-  },
-  rejected: {
-    bg: "bg-feedback-dangerLight",
-    dot: "bg-feedback-dangerBase",
-    text: "text-feedback-dangerDark",
-    label: "Recusado",
-  },
-  sent: {
-    bg: "bg-feedback-infoLight",
-    dot: "bg-feedback-infoBase",
-    text: "text-feedback-infoDark",
-    label: "Enviado",
-  },
-};
-
 export function BudgetCard({ data }: BudgetCardProps) {
-  const statusInfo = STATUS_STYLE[data.status];
   const formattedPrice = Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
@@ -50,17 +22,7 @@ export function BudgetCard({ data }: BudgetCardProps) {
       </View>
 
       <View className="flex-col items-end justify-between">
-        <View
-          className={cn(
-            "flex-row items-center gap-1 px-2 py-1 rounded-full",
-            statusInfo.bg,
-          )}
-        >
-          <View className={cn("w-2 h-2 rounded-full", statusInfo.dot)} />
-          <Text className={cn("text-text-xs font-bold", statusInfo.text)}>
-            {statusInfo.label}
-          </Text>
-        </View>
+        <StatusBadge status={data.status} />
         <Text className="text-text-md font-bold">{formattedPrice}</Text>
       </View>
     </View>
